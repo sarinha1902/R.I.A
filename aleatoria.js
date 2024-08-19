f7b31ed0cf50c9f4231ace88d436488c5e4a63d0
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-08-19 09:49:22 -0300

    tt

---
+++ aleatoria.js
@@ -0,0 +1,16 @@
+ export function aleatorio (lista){
+       const posicao = Math.floor(math.random()* lista.length);
+       return lista[posicao];
+        }
+
+
+export const perguntas = [
+    // Trecho de código suprimido
+    ]
+
+   
+        
+        import {aleatorio} from './aleatorio.js';
+
+
+        import {perguntas} from './perguntas.js';
